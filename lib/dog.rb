e8b9eea1063c8509dd4d7278f6fc4fba61df202b
Jay  Stewart <jayr.stewart@live.com>
2019-10-08 15:31:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog 
   
-  def initialize(name)
-    @name = name 
-  end
+  # def initialize(name)
+  #   @name = name 
+  # end
   
-  def name (name)
-    @name
-  end 
+  # def name (name)
+  #   @name
+  # end 
   
   def name=(new_name)
     @name = new_name
